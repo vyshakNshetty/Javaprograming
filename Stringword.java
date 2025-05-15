882122a433449f47057048350dcbc26a362de8e3
Vyshak N Shetty <vaishakvaishu285@gmail.com>
2025-05-15 18:23:29 +0530

    Stringword.java

---
+++ Stringword.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Stringword {
+    public static void main(String[] args) {
+        System.out.println("Enter the Text :");
+        Scanner vy=new Scanner(System.in);
+        String text=vy.nextLine();
+        System.out.println("Hi ,"+text);
+    }
+}
