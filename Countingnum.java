49883205137f7abef332f912d608c69595f0d22f
Vyshak N Shetty <vaishakvaishu285@gmail.com>
2025-05-15 18:29:25 +0530

    Stringword.java

---
+++ Countingnum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Countingnum {
+    public static void main(String[] args) {
+        System.out.println("Enter the Number :");
+        Scanner vy=new Scanner(System.in);
+        int n=vy.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
